7b7c3a5b0603e9638cc73fefbeeb8a26120b4d7f
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-03-04 14:50:53 +0100

    Create activities for log in, create account, games and new game

---
+++ app/src/main/java/com/example/stonesissorbag/Games.java
@@ -0,0 +1,16 @@
+package com.example.stonesissorbag;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Games extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_games);
+    }
+}
---
+++ app/src/main/java/com/example/stonesissorbag/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.stonesissorbag;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToCreateAccount(View view) {
+        Intent intent = new Intent(this, CreateAccount.class);
+        startActivity(intent);
+    }
+}
