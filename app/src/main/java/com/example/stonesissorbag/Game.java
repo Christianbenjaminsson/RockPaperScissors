a427464ad540a35d532a374e7d1e0770e388d476
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-04-01 22:02:05 +0200

    Create class

--- app/src/main/java/com/example/stonesissorbag/Game.java
+++ app/src/main/java/com/example/stonesissorbag/Game.java
@@ -1,10 +1,11 @@
 package com.example.stonesissorbag;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class game extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Game extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
