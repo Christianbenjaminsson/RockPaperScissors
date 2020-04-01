e21feae6523b9c446b76a6e0c077e8aad9f67680
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-04-01 12:20:06 +0200

    Add gif to project and Create Imageview in winner_window

---
+++ app/src/main/java/com/example/stonesissorbag/WinnerWindow.java
@@ -0,0 +1,15 @@
+package com.example.stonesissorbag;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class WinnerWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_winner_window);
+    }
+}
